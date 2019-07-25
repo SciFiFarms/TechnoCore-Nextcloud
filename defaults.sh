6f81e8f265d5ab9a3129edbc16228384636bd6a2
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2019-07-25 16:58:35 -0600

    Change nextcloud-db -> nextcloud_db & misc refator
    Mostly updating envs.

--- defaults.sh
+++ defaults.sh
@@ -2,4 +2,9 @@
 
 set_service_flag nextcloud 
 
-path_prefix NEXTCLOUD cloud
+# TODO: When I create a nextcloud instance with a prefix, it just doesn't seem 
+#       to work quite right. For one, it looks for static things on the prefix: example.com/prefix/
+#       but they don't actually exist. I think this is likely because the nginx server doesn't know
+#       it needs to serve those on a different path. Would need to figure out how to pass the prefix 
+#       to nginx. 
+#path_prefix NEXTCLOUD cloud
