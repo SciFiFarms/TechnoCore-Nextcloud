170d9f197e4c3f0d460ff0afdf401e8c0f1ed295
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2019-07-28 17:11:03 -0600

    Add trusted_proxie to allow correct remote ip to be detected.

--- config.php
+++ config.php
@@ -1,7 +1,13 @@
 <?php
 $CONFIG = array (
-    'htaccess.RewriteBase' => '/',
-  #'trusted_proxies'   => [ '10.42.0.0/24' ],
+  # This is needed to allow Nextcloud to receive visitors actual IP address rather than the ingress' IP.
+  'trusted_proxies'   => [ '10.42.0.0/24' ],
+  
+  "log_type" => "file",
+  "logfile" => "/dev/stdout",
+  "loglevel" => "1",
+
+  'htaccess.RewriteBase' => '/',
   #'overwritecondaddr' => '^10\.42\.0\..+$',
   'overwriteprotocol' => 'https',
   'overwritewebroot'  => strlen(getenv('PATH_PREFIX')) > 1 ? mb_substr(getenv('PATH_PREFIX'), 0, -1) : getenv('PATH_PREFIX'),
