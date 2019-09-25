828e94739747fe918c634f1b0a6c2561b40834fc
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2019-09-24 22:04:20 -0600

    Add dogfish to Nextcloud and improve migration logs & migration creation.

---
+++ shell-migrations/up-template.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# TODO turn this into an actually working function. . :
+#seconds=$SECONDS OR 5
+#maxtries=$MAX_TRIES OR 50
+#current=0
+#until $command;
+#do
+#    echo "$command failed. Will try again in $seconds"
+#    sleep $seconds
+#    if current >= max_tries; then
+#        echo "ERROR: $command failed $maxtries in a row. Will not try again."
+#        exit(1) 
+#    fi
+#done
+
